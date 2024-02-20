4bdf2b4c9e71b6fa75a3f80f0a94eeea5a934332
lordbagel <58605060+lordbagel42@users.noreply.github.com>
2024-02-20 16:31:01 -0700

    fix merge issues

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -16,44 +16,12 @@
 pros::Controller master(E_CONTROLLER_MASTER);
 pros::Motor intake (20);
 
-void opcontrol()
-{
-	// loop forever
-	while (true)
-	{
-		// get left y and right x positions
-		int leftY = master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
-		int rightX = master.get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
-
-		// move the robot
-		chassis.curvature(leftY, rightX);
-		// set port 20 motor to 200 rpm if the bumper is hit
-		if (master.get_digital(E_CONTROLLER_DIGITAL_R1)) {
-			intake = -127;
-		}
-		else if (master.get_digital(E_CONTROLLER_DIGITAL_R2)) {
-			intake = 127;
-		}
-		else {
-			intake = 0;
-		}
-
-		// delay so we don't destroy the CPU
-		pros::delay(10);
-	}
-}#include "main.h"
-#include "headers/globals.hpp"
-#include "headers/odometry.hpp"
-#include "headers/opcontrol.hpp"
-#include "headers/screensim.hpp"
-
 /**
  * Master controls left and right drive
  * T value is 12.4
  * Using curvature drive. Left stick controls speed, right stick controls the radius of a turn. Speed does not affect radius.
  *
  */
-
 void opcontrol()
 {
 	// loop forever
@@ -65,6 +33,16 @@ void opcontrol()
 
 		// move the robot
 		chassis.curvature(leftY, rightX, 12.4);
+		// intake if R1 pressed, spit if R2 pressed
+		if (master.get_digital(E_CONTROLLER_DIGITAL_R1)) {
+			intake = -127;
+		}
+		else if (master.get_digital(E_CONTROLLER_DIGITAL_R2)) {
+			intake = 127;
+		}
+		else {
+			intake = 0;
+		}
 
 		// delay so we don't destroy the CPU
 		pros::delay(10);
