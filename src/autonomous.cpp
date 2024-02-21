ffd39ade25e053be899f89a99ac910c2cd16aaec
lordbagel <58605060+lordbagel42@users.noreply.github.com>
2024-02-20 17:07:24 -0700

    empty auton template

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -4,7 +4,7 @@
 #include "headers/odometry.hpp"
 #include "autoSelect/selection.h"
 
-ASSET(example_txt);
+ASSET(path1_txt);
 void autonomous()
 {
     if (selector::auton == 1)
@@ -16,4 +16,32 @@ void autonomous()
 
         chassis.moveToPose(0, 50, 90, 3000);
     }
+
+    else if (selector::auton == 2) // run auton for Back Red
+    {
+    }
+
+    else if (selector::auton == 3) // no auton for Red
+    {
+    }
+
+    else if (selector::auton == -1) // run auton for Front Blue
+    {
+    }
+    
+    else if (selector::auton == -2) // run auton for Back Blue
+    {
+    }
+
+    else if (selector::auton == -3) // no auton for Blue
+    {
+    }
+
+    else if (selector::auton == 0) // skills auton
+    {
+    }
+
+    else // no auton
+    {
+    };
 }
